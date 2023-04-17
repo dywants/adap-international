292d7d62e04ee91b6f4e5a98cc1c84215a342122
Cyrille Thibaut BAYONG <dywantscm@gmail.com>
2023-04-17 16:42:21 +0100

    refactoring

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -1,30 +1,29 @@
 <!doctype html>
 <html lang="fr">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+        content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>ADAP International</title>
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
+
 <body class="leading-normal tracking-normal flex flex-col min-h-screen ">
 
-<section class="w-full inset-x-0 z-1600">
-    <x-front.navbar></x-front.navbar>
-{{--    @yield('header')--}}
-</section>
-<!--Header-->
+    <section class="w-full inset-x-0 z-1600">
+        <x-front.navbar></x-front.navbar>
+    </section>
+    <!--Header-->
 
-<!--Container-->
+    <!--Container-->
     <main class="relative bg-dark-100 grow overflow-hidden">
-{{--        <div class="absolute top-0 right-0 z-1400 hidden">--}}
-{{--            @include('includes.flash-message')--}}
-{{--        </div>--}}
         @yield('content')
     </main>
 
-<x-front.footer/>
-@yield('extra-js')
+    <x-front.footer />
+    @yield('extra-js')
 </body>
+
 </html>
